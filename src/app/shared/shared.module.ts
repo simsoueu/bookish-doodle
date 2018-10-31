3306c09569bbf4f1b8f56f8c12b1f2a6945717d5
João Antonio <joaoapenas@gmail.com>
2018-10-31 10:57:15 -0300

    fixed linting stage

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
 import { CodeComponent } from './code/code.component';
-import { CodeHighlightService } from "./code/code-highlight.service";
+import { CodeHighlightService } from './code/code-highlight.service';
 
 @NgModule({
     imports: [CommonModule, RouterModule, NgbModule],
